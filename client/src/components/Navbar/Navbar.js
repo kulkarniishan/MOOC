93802e40974b33f5132c126f60a3ffe0df510442
Wase <wasekhan558@gmail.com>
2021-12-04 03:46:58 +0530

    edit client

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-export default function Navbar() {
-    return (
-        <div>
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">MOOC</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav m-auto">
+            <li class="nav-item">
+              <a className="nav-link active text-white text-uppercase me-3" aria-current="page" href="#">Home&nbsp;<i class="fas fa-home"></i></a>
+            </li>
+            <li className="nav-item">
+              <a class="nav-link text-white text-uppercase me-3" href="#">About Us</a>
+            </li>
+            <li className="nav-item">
+              <a class="nav-link text-white text-uppercase" href="#">Contact Us</a>
+            </li>
             
+          </ul>
+          <form inline >
+            <button variant="primary" href="/login" className="btn btn-outline-primary me-3">Login</button>
+            <button variant="outline-primary" href="/register" className="btn btn-outline-primary me-5">Register</button>
+          </form>
         </div>
-    )
+      </div>
+    </nav>
+  )
 }
+
+export default Navbar;
