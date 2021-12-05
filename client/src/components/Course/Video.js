e418ba09e745b11ed2460f486be5d82bd7e65a00
Wase <wasekhan558@gmail.com>
2021-12-05 15:48:06 +0530

    e

--- client/src/components/Course/Video.js
+++ client/src/components/Course/Video.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { Component} from 'react'
 
 export default function Video(props) {
     console.log(props);
     return (
-        <div className='bg-red'>
-            Video
-            dkwqkld
-            qwdqwd
-            wdqwdddddddd
-            <hr />
-            dqwidew
-            <hr />qwdqw
-        </div>
-    )
+        <div class="jumbotron">
+            
+            <h2 class="text-center">Basics of Python</h2>
+                
+            <div align="center" className="embed-responsive embed-responsive-16by9">
+                                <video autoplay loop className="embed-responsive-item">
+                                    <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+                                </video>
+                            </div>
+               
+            </div>
+
+    );
 }
