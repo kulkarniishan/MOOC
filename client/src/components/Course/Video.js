2e808f52d9a21ffe90d6e2710707cde27ca8ed00
Ishan <ishan.kulkarni@somaiya.edu>
2021-12-05 14:54:02 +0530

    FEATURE: video page done

--- client/src/components/Course/Video.js
+++ client/src/components/Course/Video.js
@@ -3,14 +3,18 @@ import React from 'react'
 export default function Video(props) {
     console.log(props);
     return (
-        <div className='bg-red'>
-            Video
-            dkwqkld
-            qwdqwd
-            wdqwdddddddd
-            <hr />
-            dqwidew
-            <hr />qwdqw
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="h1">Title</div>
+                </div>
+                <hr />
+                <video width="100%" controls autoplay>
+                    <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+                </video>
+            </div>
+
+
         </div>
     )
 }
